a73b3715c427e6837b986ffda299ebc82fb2b0df
Muchacos <muchacos@ya.ru>
2021-02-23 12:17:49 +0300

    Initial commit

---
+++ Source/RedGravity/RedGravityGameModeBase.h
@@ -0,0 +1,17 @@
+// Copyright Epic Games, Inc. All Rights Reserved.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "GameFramework/GameModeBase.h"
+#include "RedGravityGameModeBase.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class REDGRAVITY_API ARedGravityGameModeBase : public AGameModeBase
+{
+	GENERATED_BODY()
+	
+};
